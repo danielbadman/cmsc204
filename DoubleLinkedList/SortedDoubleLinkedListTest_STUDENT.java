123e0b2576a97187e0cc9cf7dd9a61c4199a3650
danielbadman <114884326+danielbadman@users.noreply.github.com>
2024-03-06 22:39:13 -0500

    Update SortedDoubleLinkedListTest_STUDENT.java

--- DoubleLinkedList/SortedDoubleLinkedListTest_STUDENT.java
+++ DoubleLinkedList/SortedDoubleLinkedListTest_STUDENT.java
@@ -1,45 +1,80 @@
 
 /**
- * GAF Test class for SortedDoubleLinkedList
- * 
- * @author Farnaz Eivazi
- * @version 1/29/2024
+ * You must implement the following test case methods
  */
-
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.ArrayList;
 import java.util.Comparator;
-import java.util.GregorianCalendar;
-import java.util.Iterator;
 import java.util.ListIterator;
-import java.util.NoSuchElementException;
 
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
-public class SortedDoubleLinkedList_GFA_Test {
-	SortedDoubleLinkedList<String> sortedLinkedString;
-	StringComparator comparator = new StringComparator();
+class SortedDoubleLinkedListTest_STUDENT {
 
-	@Before
-	public void setUp() throws Exception {
-		sortedLinkedString = new SortedDoubleLinkedList<>(comparator);
+	SortedDoubleLinkedList<String> sortedList;
+	
+	@BeforeEach
+	void setUp() throws Exception {
+		sortedList = new SortedDoubleLinkedList<String>(new StringComparator());
 	}
 
-	@After
-	public void tearDown() throws Exception {
-		sortedLinkedString = null;
+	@AfterEach
+	void tearDown() throws Exception {
+		sortedList = null;
 	}
 
 	@Test
-	public void testAddToEnd() {
-		try {
-			sortedLinkedString.addToEnd("Hello");
-		} catch (UnsupportedOperationException e) {
-			assertEquals(e.getMessage(), "Invalid operation for sorted list");
-		}
+	void testIterator() {
+		sortedList.add("Adam");
+		sortedList.add("Flynn");
+		sortedList.add("Eric");
+		sortedList.add("Yvon");
+		sortedList.add("Jasmine");
+		
+		ListIterator iter = sortedList.iterator();
+		
+		assertTrue(iter.hasNext());
+		assertEquals("Adam", iter.next());
+		assertEquals("Eric", iter.next());
+		assertEquals("Flynn", iter.next());
+		assertEquals("Jasmine", iter.next());
+		assertEquals("Yvon", iter.next());
+		assertFalse(iter.hasNext());
+		assertEquals("Jasmine", iter.previous());
+		assertEquals("Flynn", iter.previous());
+		assertEquals("Eric", iter.previous());
+		assertEquals("Adam", iter.previous());
+		assertFalse(iter.hasPrevious());
+	}
+
+	@Test
+	void testRemove() {
+		sortedList.add("J Cole");
+		sortedList.add("Kendrick Lamar");
+		sortedList.add("Kanye West");
+		sortedList.add("Earl Sweatshirt");
+		
+		assertEquals("Kanye West", sortedList.remove("Kanye West", new StringComparator()).data);
+		assertEquals(3, sortedList.getSize());
+	}
+
+	@Test
+	void testAdd() {
+		sortedList.add("Icedancer");
+		sortedList.add("Red Light");
+		sortedList.add("Gluee");
+		sortedList.add("Crest");
+		
+		assertEquals(4, sortedList.getSize());
+		ArrayList<String> arrList = sortedList.toArrayList();
+		
+		assertEquals("Crest", arrList.get(0));
+		assertEquals("Gluee", arrList.get(1));
+		assertEquals("Icedancer", arrList.get(2));
+		assertEquals("Red Light", arrList.get(3));
 	}
 
 	private class StringComparator implements Comparator<String> {
@@ -48,6 +83,6 @@ public class SortedDoubleLinkedList_GFA_Test {
 		public int compare(String arg0, String arg1) {
 			return arg0.compareTo(arg1);
 		}
-	}
 
+	}
 }
